461a2c9f59d066fb1ed8fe77b12ed0b72b4c84c2
Rita Bradley <contact@ritabradley.com>
2019-04-26 09:34:45 -0700

    app scaffolding

---
+++ src/components/SeasonDisplay.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SeasonDisplay = () => {
+  return (
+    <div>
+      <h4>Something about the season</h4>
+    </div>
+  );
+};
+
+export default SeasonDisplay;
---
+++ src/containers/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import SeasonDisplay from '../components/SeasonDisplay';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <SeasonDisplay />
+      </div>
+    );
+  }
+}

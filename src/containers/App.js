13e18eba52c9dcc9d7523a068fae49bb12f87ea7
Rita Bradley <contact@ritabradley.com>
2019-04-27 10:24:48 -0700

    render latitude to screen

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,10 +2,25 @@ import React, { Component } from 'react';
 import SeasonDisplay from '../components/SeasonDisplay';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null, errorMessage: '' };
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message }),
+    );
+  }
+
   render() {
     return (
       <div>
-        <SeasonDisplay />
+        {!this.state.errorMessage ? (
+          <h1>Latitude: {!this.state.lat ? 'Loading...' : this.state.lat}</h1>
+        ) : (
+          <h1>Error: {this.state.errorMessage}</h1>
+        )}
       </div>
     );
   }

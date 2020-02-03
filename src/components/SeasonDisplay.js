dd5de8204d7c3b07ee8f601944eb5e542cebdb59
Rita Bradley <contact@ritabradley.com>
2020-02-03 13:53:51 -0800

    cleaned up with destructuring

--- src/components/SeasonDisplay.js
+++ src/components/SeasonDisplay.js
@@ -2,38 +2,37 @@ import './SeasonDisplay.css';
 import React from 'react';
 
 const seasonConfig = {
-  summer: {
-    displayText: "Sun's out, buns out!",
-    iconClass: 'sun outline',
-    iconColor: 'orange',
-  },
-  winter: {
-    displayText: 'I feel an icicle forming under my nose!',
-    iconClass: 'snowflake outline',
-    iconColor: 'blue',
-  },
+    summer: {
+        displayText: "Sun's out, buns out!",
+        iconClass: 'sun outline',
+        iconColor: 'orange'
+    },
+    winter: {
+        displayText: 'I feel an icicle forming under my nose!',
+        iconClass: 'snowflake outline',
+        iconColor: 'blue'
+    }
 };
 
 const getSeason = (lat, month) => {
-  if (month > 2 && month < 9) {
-    return lat > 0 ? 'summer' : 'winter';
-  } else {
-    return lat < 0 ? 'winter' : 'summer';
-  }
+    if (month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat < 0 ? 'winter' : 'summer';
+    }
 };
 
-const SeasonDisplay = props => {
-  const { lat } = props;
-  const season = getSeason(lat, new Date().getMonth());
-  const { displayText, iconClass, iconColor } = seasonConfig[season];
+const SeasonDisplay = ({ lat }) => {
+    const season = getSeason(lat, new Date().getMonth());
+    const { displayText, iconClass, iconColor } = seasonConfig[season];
 
-  return (
-    <div className={`season-display ${season}`}>
-      <i className={`icon-left massive ${iconColor} ${iconClass} icon`} />
-      <h1>{displayText}</h1>
-      <i className={`icon-right massive ${iconColor} ${iconClass} icon`} />
-    </div>
-  );
+    return (
+        <div className={`season-display ${season}`}>
+            <i className={`icon-left massive ${iconColor} ${iconClass} icon`} />
+            <h1>{displayText}</h1>
+            <i className={`icon-right massive ${iconColor} ${iconClass} icon`} />
+        </div>
+    );
 };
 
 export default SeasonDisplay;
--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,15 +1,12 @@
 import './Spinner.css';
 import React from 'react';
 
-const Spinner = props => {
-  const { loadingMessage } = props;
-  return (
-    <div className='ui active dimmer'>
-      <div className='ui huge text loader'>
-        {loadingMessage || 'Fetching your location...'}
-      </div>
-    </div>
-  );
+const Spinner = ({ loadingMessage }) => {
+    return (
+        <div className='ui active dimmer'>
+            <div className='ui huge text loader'>{loadingMessage || 'Fetching your location...'}</div>
+        </div>
+    );
 };
 
 export default Spinner;
